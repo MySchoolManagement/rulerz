1b28a4e4592d34ea5128e46cbe5ee36760f7476d
Iain Mckay <me@iainmckay.co.uk>
2018-01-09 14:43:18 +0100

    Fixes a bug where you cannot use a runtime operator in a SQL query

--- src/Target/AbstractCompilationTarget.php
+++ src/Target/AbstractCompilationTarget.php
@@ -1,11 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace RulerZ\Target;
 
 use RulerZ\Compiler\Context;
 use RulerZ\Compiler\CompilationTarget;
 use RulerZ\Model;
+use RulerZ\Target\Operators\CompileTimeOperator;
 use RulerZ\Target\Operators\Definitions as OperatorsDefinitions;
+use RulerZ\Target\Operators\Definitions;
 
 /**
  * Generic visitor intended to be extended.
@@ -40,11 +44,15 @@ abstract class AbstractCompilationTarget implements CompilationTarget
     /**
      * {@inheritdoc}
      */
-    public function compile(Model\Rule $rule, Context $compilationContext)
+    public function compile(Model\Rule $rule, Context $compilationContext): Model\Executor
     {
         $visitor = $this->createVisitor($compilationContext);
         $compiledCode = $visitor->visit($rule);
 
+        if ($compiledCode instanceof CompileTimeOperator) {
+            $compiledCode = $compiledCode->format(false);
+        }
+        
         return new Model\Executor(
             $this->getExecutorTraits(),
             $compiledCode,
@@ -55,7 +63,7 @@ abstract class AbstractCompilationTarget implements CompilationTarget
     /**
      * {@inheritdoc}
      */
-    public function createCompilationContext($target)
+    public function createCompilationContext($target): Context
     {
         return new Context();
     }
@@ -63,7 +71,7 @@ abstract class AbstractCompilationTarget implements CompilationTarget
     /**
      * {@inheritdoc}
      */
-    public function defineOperator($name, callable $transformer)
+    public function defineOperator(string $name, callable $transformer): void
     {
         $this->customOperators->defineOperator($name, $transformer);
     }
@@ -71,7 +79,7 @@ abstract class AbstractCompilationTarget implements CompilationTarget
     /**
      * {@inheritdoc}
      */
-    public function defineInlineOperator($name, callable $transformer)
+    public function defineInlineOperator(string $name, callable $transformer): void
     {
         $this->customOperators->defineInlineOperator($name, $transformer);
     }
@@ -79,7 +87,7 @@ abstract class AbstractCompilationTarget implements CompilationTarget
     /**
      * {@inheritdoc}
      */
-    public function getOperators()
+    public function getOperators(): Definitions
     {
         return $this->customOperators;
     }
@@ -87,7 +95,7 @@ abstract class AbstractCompilationTarget implements CompilationTarget
     /**
      * {@inheritdoc}
      */
-    public function getRuleIdentifierHint($rule, Context $context)
+    public function getRuleIdentifierHint(string $rule, Context $context): string
     {
         return '';
     }
